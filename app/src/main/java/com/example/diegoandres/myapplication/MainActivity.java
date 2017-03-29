e3006faea4b9404e1861e76cd88b5f91b3efc498
diego <dalh779@gmail.com>
2017-03-28 20:08:06 -0500

    se modifico el archivo con el fin de crear la accion del boton Button

--- app/src/main/java/com/example/diegoandres/myapplication/MainActivity.java
+++ app/src/main/java/com/example/diegoandres/myapplication/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.diegoandres.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView Vista1;
+    Button boton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Vista1 = (TextView) findViewById(R.id.view1);
+        boton1 = (Button) findViewById(R.id.button);
+        boton1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Vista1.setText("Cambio el texto oprimiendo el boton");
     }
 }
